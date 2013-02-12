1a8d6ab16cabf74b08569a6ba6892682d5ff8b26
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-02-12 15:27:43 +0200

    Minor parsing changes

--- pull.py
+++ pull.py
@@ -5,20 +5,25 @@ import argparse
 import mechanize
 import csv
 import sys
+import re
 import logging
 logging.basicConfig(level=logging.INFO)
 
-headers = ["Year", "ModuleName", "Group", "Language", "Activity", "YearPhase", 
+headers = ["Year", "ModuleName", "Group", "Language", "Activity", "YearPhase",
            "Day", "Time", "Venue", "ENGcode"]
 
 parser = argparse.ArgumentParser(description="Download timetable data from UP web")
-parser.add_argument("url", default="http://uptt.up.ac.za:90/eng_timetable.html", 
+parser.add_argument("url", default="http://uptt.up.ac.za:90/eng_timetable.html",
                     nargs="?", type=str)
-parser.add_argument('--outfile', '-o', default=sys.stdout, 
+parser.add_argument('--outfile', '-o', default=sys.stdout,
                     type=argparse.FileType('w'))
 
 def engcodesplit(codestr):
-    return (codestr[i:i+2] for i in range(0, len(codestr), 2)) 
+    return (codestr[i:i+2] for i in range(0, len(codestr), 2))
+
+def filterchars(thestring, removechars):
+    return re.sub('[' + removechars + ']', '', thestring)
+
 
 def parse(row):
     logging.debug(row)
@@ -27,18 +32,19 @@ def parse(row):
         Year, ModuleName, Group, Language, Activity = code.split('/')
         Time = From[0:-3] + "-" + To[0:-5] + '20'
         return([Year, ModuleName, Group, Language, Activity, YearPhase,
-               Day, Time, Venue, ENGcodes])
+                Day, Time, Venue, filterchars(ENGcodes, "'")])
     except:
         logging.error("Problem with this record: " + str(row))
-        
+
+
 def downloadtable(url):
     br = mechanize.urlopen(url)
     soup = BeautifulSoup.BeautifulSoup(br.read())
-    
+
     table = soup.find(id='myTable')
     for row in table('tr'):
         yield [i.string for i in row('td')]
-            
+
 if __name__ == "__main__":
     args = parser.parse_args()
     out = csv.writer(args.outfile)
