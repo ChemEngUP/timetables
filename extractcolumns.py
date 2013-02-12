afe73d16bfdf0a3e0a140373da259c3fea4039e9
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-02-12 15:26:36 +0200

    Better support for argument parsing in extractcolumns

--- extractcolumns.py
+++ extractcolumns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Author: Carl Sandrock 
+# Author: Carl Sandrock
 # Date: 20120206
 
 import csv
@@ -10,11 +10,11 @@ import logging
 import os.path
 
 parser = argparse.ArgumentParser(description="Extract certain named columns from a CSV file")
-parser.add_argument('infile', default='-', nargs="?",
+parser.add_argument('infile', default=[sys.stdin], nargs="*",
                     type=argparse.FileType('r'))
-parser.add_argument('--outfile', '-o', default=sys.stdout, 
+parser.add_argument('--outfile', '-o', default=sys.stdout,
                     type=argparse.FileType('w'))
-parser.add_argument('--columns', '-c', nargs='+', type=str, 
+parser.add_argument('--columns', '-c', nargs=1, type=str,
                     help="Column names to retain")
 parser.add_argument('--headerfile', '-f', type=str,
                     help="Read column names from a file - one name per line")
@@ -25,28 +25,32 @@ parser.add_argument('--sort', '-s', action="store_true", default=False)
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    inf = csv.DictReader(args.infile)
-    incolumns = inf.fieldnames
-    
     outf = csv.writer(args.outfile)
 
-    if args.listheaders:
-        outf.writerows(enumerate(incolumns))
+    if args.columns:
+        columns = args.columns[0].split(',')
     else:
-        if args.columns:
-            columns = args.columns
-        else:
-            if args.headerfile:
-                if os.path.exists(args.headerfile):
-                    columns = [w.strip() for w in open(args.headerfile)]
-                else:
-                    logging.error("Bad header file")
-                    sys.exit(2)
+        if args.headerfile:
+            if os.path.exists(args.headerfile):
+                columns = [w.strip() for w in open(args.headerfile)]
             else:
-                columns = incolumns
-                
+                logging.error("Bad header file")
+                sys.exit(2)
+        else:
+            columns = None
+
+    if not args.listheaders:
         outf.writerow(columns)
-        data = [[row[column] for column in columns] for row in inf]
-        if args.sort: 
-            data.sort()
-        outf.writerows(data)
+        data = []
+
+    for infile in args.infile:
+        inf = csv.DictReader(infile)
+        incolumns = inf.fieldnames
+        if args.listheaders:
+            outf.writerows(enumerate(incolumns))
+        else:
+            data += [[row[column] for column in columns] for row in inf]
+
+    if args.sort:
+        data.sort()
+    outf.writerows(data)
