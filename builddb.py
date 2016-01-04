d64477a22966ee8d9ba16918edb081a3c0dda69e
Carl Sandrock <carl.sandrock@up.ac.za>
2016-01-04 17:31:22 +0200

    Add venue list

--- builddb.py
+++ builddb.py
@@ -17,4 +17,9 @@ df['Year'] = df.ENGcode.str[1].apply(int)
 
 df.to_sql('timetable', db, if_exists='replace', index=False)
 
+venues = set(v.strip() for v in sum((v.replace(" and ", "; ").split(';')
+                                     for v in df.Venue.unique()), []) if len(v) > 3)
+
+pandas.Series(list(venues), name="name").to_sql('venues', db, if_exists='replace', index=False)
+
 # TODO: should I close the db?
